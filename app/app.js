e9dec08259efd9bad1e3c6dff2883172d6d86cb3
Josue <josketres@gmail.com>
2013-08-27 19:27:24 +0200

    Express hello-world

--- app/app.js
+++ app/app.js
@@ -1,2 +1,8 @@
 var express = require('express');
 var app = express();
+app.get('/hello.txt', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
